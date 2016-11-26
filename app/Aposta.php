13d7e96488395669fa02540c5c23042a44cee522
alex-ubuntu-casa <alexbenincasa@ymail.com>
2016-11-26 18:26:22 -0200

    Model e migration de Aposta

--- app/Aposta.php
+++ app/Aposta.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aposta extends Model
 {
-    //
+	public static function store($dados){
+		$obj = new Aposta;
+		
+		$obj->usuarioId =			$dados->usuarioId;
+		$obj->jogoId =				$dados->jogoId; 
+		$obj->numero_concurso =		$dados->numero_concurso;
+		$obj->acertos =				$dados->acertos;
+
+		$obj->save();
+
+		return $obj->id;
+	}
+	public static function update($dados){
+		$obj = Pagamento::find($dados->pagamentoId);
+		
+		$obj->usuarioId =			$dados->usuarioId;
+		$obj->jogoId =				$dados->jogoId; 
+		$obj->numero_concurso =		$dados->numero_concurso;
+		$obj->acertos =				$dados->acertos;
+
+		$obj->save();
+	}
+	public static function storeOrUpdate($dados){
+		if($dados->apostaId == '') 
+			return Pagamento::store($dados);
+		else {
+			Pagamento::update($dados); 
+			return $dados->apostaId; 
+		}
+	}
 }
--- database/migrations/2016_11_26_194341_create_apostas_table.php
+++ database/migrations/2016_11_26_194341_create_apostas_table.php
@@ -6,26 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateApostasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('apostas', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('apostas', function (Blueprint $table) {
+			$table->increments('id'); 
+			$table->int('usuarioId'); 
+			$table->int('jogoId'); 
+			$table->string('numero_concurso'); 
+			$table->int('acertos'); 
+			$table->timestamps(); 
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('apostas');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('apostas');
+	}
 }
