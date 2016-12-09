30ecc7e29c1f6b570cc46bf9dbaac3109e952a9f
alex-ubuntu-casa <alexbenincasa@ymail.com>
2016-12-09 02:07:37 -0200

    configurações de email funcionando

--- config/mail.php
+++ config/mail.php
@@ -5,11 +5,12 @@ return [
     'host' => env('MAIL_HOST'),
     'port' => env('MAIL_PORT'),
     'from' => [
-        'address' => 'querojogar@alexbnkz.com',
+        'address' => 'querojogar.apostacerta@outlook.com',
         'name' => 'Aposta Certa',
     ],
     'encryption' => env('MAIL_ENCRYPTION'),
     'username' => env('MAIL_USERNAME'),
     'password' => env('MAIL_PASSWORD'),
     'sendmail' => '/usr/sbin/sendmail -bs',
+    'pretend' => false,
 ];
