0feb23efb977b9b744956046e8b19770381db1ba
alex-ubuntu-casa <alexbenincasa@ymail.com>
2016-12-21 20:11:29 -0200

    Mensagens padrão em pt-br

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,19 @@
 @extends('layouts.navbar')
 
 @section('content')
-<div class="flex-center position-ref">
-
-	<div class="content">
-		<div class="title m-b-md">
-			Aposta Certa
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="title m-b-md center">
+					Aposta Certa
+				</div>
+				<h3 class="center">Inteligência Matemática pra você apostar certinho!</h3>
+				<div class="panel-body">
+					Se você precisa calcular probabilidades pra fazer apostas 
+					mas dormiu nas aulas de matemática do colegial, seus problemas acabaram!
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
