00e6045efd9e04f6795da28b764806768953874e
alex-ubuntu-casa <alexbenincasa@ymail.com>
2016-12-22 15:55:45 -0200

    Dropdown Administrador

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,51 +1,74 @@
 @section('logout')
-	<li><a href="{{ url('/logout') }}" 
+	<li title="Sair"><a href="{{ url('/logout') }}" 
 		onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-		Logout</a>
+		Sair</a>
 		<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
 	</li>
 @endsection
 
 @section('navegacao')
 	@if (Auth::guest())
-	<li><a href="{{ url('/login') }}">Login</a></li>
-	<li><a href="{{ url('/register') }}">Registrar</a></li>
+	<li title="Login"><a href="{{ url('/login') }}">Login</a></li>
+	<li title="Registrar"><a href="{{ url('/register') }}">Registrar</a></li>
 	@else
-	<li><a href="{{ url('/home') }}">Concursos</a></li>
-	<li><a href="#">Minhas Apostas</a></li>
+	<li title="Concursos"><a href="{{ url('/home') }}">Concursos</a></li>
+	<li title="Minhas Apostas"><a href="#">Minhas Apostas</a></li>
 	@endif
 @endsection
 @section('usuario')
-	<li><a href="#">Dados de cadastro</a></li>
-	<li><a href="#">Trocar Senha</a></li>
+	<li title="Dados de cadastro"><a href="#">Dados de cadastro</a></li>
+	<li title="Trocar Senha"><a href="#">Trocar Senha</a></li>
 	<li class="divider"></li>
 	@yield('logout')
 @endsection
+@section('administrador')
+	<li><a href="#">Configurações</a></li>
+@endsection
 
-@if (!Auth::guest())
+@if (!Auth::guest()) 
 <ul id="app-menu" class="dropdown-content">
 	@yield('usuario')
 </ul>
+	@if(Auth::user()->privilegio = 'administrador')
+	<ul id="app-adm" class="dropdown-content">
+		@yield('administrador')
+	</ul>
+	@endif
 @endif
 
 <nav>
 	<div class="nav-wrapper">
-		<a href="/" class="brand-logo center">Aposta Certa</a>
+		<a href="/" class="brand-logo center" title="Aposta Certa">Aposta Certa</a>
 		<ul class="right hide-on-med-and-down">
-			@yield('navegacao')
-			@if (!Auth::guest())
-			<li><a class="dropdown-button" href="#!" data-activates="app-menu">
-			{{ Auth::user()->name }}<i class="material-icons right">arrow_drop_down</i></a></li>
+		@yield('navegacao')
+
+		@if (!Auth::guest())
+			<li title="{{ Auth::user()->name }}"><a class="dropdown-button" href="#!" data-activates="app-menu">
+			<i class="material-icons right" style="margin-left: 0px;">arrow_drop_down</i>{{ Auth::user()->name }}</a></li>
+
+			@if(Auth::user()->privilegio = 'administrador')
+				<li title="Configurações"><a class="dropdown-button" href="#!" data-activates="app-adm">
+				<i class="material-icons">more_vert</i></a></li>
 			@endif
+		@endif
 		</ul>
 
 		<a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
 		<ul class="side-nav" id="mobile-menu">
-			@yield('navegacao')
-			@if (!Auth::guest())
-			<li><a href="#">{{ Auth::user()->name }}</a></li>
-			@yield('usuario')
+		@if (!Auth::guest())
+		<li class="teal darken-2 white-text"><b>Menu</b></li>
+		@endif
+
+		@yield('navegacao')
+
+		@if (!Auth::guest())
+			<li class="divider"></li>
+			@if(Auth::user()->privilegio = 'administrador')
+				@yield('administrador')
+				<li class="divider"></li>
 			@endif
+			@yield('usuario')
+		@endif
 		</ul>
 	</div>
 </nav>
